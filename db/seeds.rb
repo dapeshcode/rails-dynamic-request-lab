a8c83a4db158eee4af66fdf3ba8dd2e241246747
dapeshcode <ampeccia@gmail.com>
2021-02-23 15:38:37 -0600

    commit tree 2d68e132d7078a1586744b206c44c50ae69a8e68

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Student", last_name: "1")
+
+Student.create(first_name: "Student", last_name: "2")
